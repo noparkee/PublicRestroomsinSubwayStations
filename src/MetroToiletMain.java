6802f12ff25e69d1b348bd2386fa7854ca300e3e
unknown <noparkee@naver.com>
2018-11-11 01:08:55 +0900

    add UI

--- src/MetroToiletMain.java
+++ src/MetroToiletMain.java
@@ -1,17 +1,88 @@
 import java.util.*;
 import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class StoreStation {
+	static String[] makeArrayMetro3() {
+		String metro3[] = new String[43];
+		File fileL3 = new File("metro3.txt");
+		try {
+			Scanner mt3 = new Scanner(fileL3);
+			while(mt3.hasNext()) {
+				for(int i =0; i<metro3.length; i++)
+					metro3[i] = mt3.nextLine();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일을 가져오던 중 오류가 발생했습니다.");
+			System.exit(0);
+		}
+		return metro3;
+	}
+	
+	static String[] makeArrayMetro6() {
+		String metro6[] = new String[39];
+		File fileL6 = new File("metro6.txt");
+		try {
+			Scanner mt6 = new Scanner(fileL6);
+			while(mt6.hasNext()) {
+				for(int i =0; i<metro6.length; i++)
+					metro6[i] = mt6.nextLine();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일을 가져오던 중 오류가 발생했습니다.");
+			System.exit(0);
+		}
+		return metro6;
+	}
+}
+
+
+class UI extends JFrame{
+	
+	public UI() {
+		
+		String metroLine[] = {"3호선","6호선"};
+		String metro3[] = StoreStation.makeArrayMetro3();
+		String metro6[] = StoreStation.makeArrayMetro6();
+		
+		Container contentPane = getContentPane();
+		contentPane.setLayout(null);
+		
+				
+		JComboBox<String> linenum = new JComboBox<String>(metroLine); //호선 선택
+		linenum.setBounds(50, 50, 70, 30);
+		linenum.setOpaque(true);
+		contentPane.add(linenum);
+	
+		JLabel stname = new JLabel();
+		stname.setText("역:");
+		stname.setBounds(150, 50, 30, 30);
+		contentPane.add(stname);
+		
+		JTextField tf = new JTextField(10); //역 이름 입력
+		tf.setBounds(180, 50, 250, 30);
+		contentPane.add(tf);
+		String station = tf.getText();
+		
+		//panel.add(tf);
+		
+		setTitle("Toilet in Station");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setSize(500, 300);
+		
+		setVisible(true);
+		
+	}
+}
+
+
 
 class Search{
-	/*public void question() {
-		Scanner a = new Scanner(System.in);
-		System.out.print("현재 탑승한 지하철 호선: ");
-		int metroline = a.nextInt();
-		System.out.print("현재 위치한 역: ");
-		String station = a.next();
-		System.out.print("상행선(내선) or 하행선(외선): ");
-		String direction = a.next();
-	}*/
-	public static void searchClosest(int metroline, String station, String direction, String [] metro, int [] toiletinst) {
+	
+	static void searchClosest(int metroline, String station, String direction, String [] metro, int [] toiletinst) {
 		int n;
 		int min = 100;
 
@@ -57,8 +128,12 @@ public class MetroToiletMain{
 	
 	public static void main(String[] args) {
 		
-		String metro3[] = new String[43];
-		String metro6[] = new String[39];
+		new UI();
+		String metro3[] = StoreStation.makeArrayMetro3();
+		String metro6[] = StoreStation.makeArrayMetro6();
+		
+		/*static String metro3[] = new String[43];
+		static String metro6[] = new String[39];
 
 		File fileL3 = new File("metro3.txt");
 		try {
@@ -82,16 +157,20 @@ public class MetroToiletMain{
 		} catch (FileNotFoundException e) {
 			System.out.println("파일을 가져오던 중 오류가 발생했습니다.");
 			System.exit(0);
-		}
+		}*/
 		//-----------------↑3호선, 6호선 배열 정리↑---------------------------------
 		
-		Scanner a = new Scanner(System.in);
+		/*Scanner a = new Scanner(System.in);
 		System.out.print("현재 탑승한 지하철 호선: ");
 		int metroline = a.nextInt();
 		System.out.print("현재 위치한 역: ");
 		String station = a.next();
 		System.out.print("상행선(내선) or 하행선(외선): ");
 		String direction = a.next();
+		*/
+		
+		/*if(metroline == 3)
+			Search.searchClosest(metroline, station, direction, metro3, toiletinst3);*/
 
 		
 
