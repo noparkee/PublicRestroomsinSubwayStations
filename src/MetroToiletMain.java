8966c583faa965243f4cca5402f30e6155e3ed38
unknown <noparkee@naver.com>
2018-11-14 21:06:28 +0900

    add event

--- src/MetroToiletMain.java
+++ src/MetroToiletMain.java
@@ -20,7 +20,7 @@ class StoreStation {
 		}
 		return metro3;
 	}
-	
+
 	static String[] makeArrayMetro6() {
 		String metro6[] = new String[39];
 		File fileL6 = new File("metro6.txt");
@@ -40,79 +40,131 @@ class StoreStation {
 
 
 class UI extends JFrame{
+	static int toiletinst3[] = {5, 11, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 35, 37, 40, 41, 42}; 
+	static int toiletinst6[] = {9, 10, 22, 28, 29, 30, 31, 35};
+	
+	String enteredStation;
+	int selectedLine;
+	int selectedDirection;
 	
 	public UI() {
 		
-		String metroLine[] = {"3호선","6호선"};
-		String metroDirection[] = {"상행선", "하행선", "내선순환", "외선순환"};
+		String metroLine[] = {"호선","3호선","6호선"};
+		String metroDirection[] = {"방향","상행선", "하행선", "내선순환", "외선순환"};
 		String metro3[] = StoreStation.makeArrayMetro3();
 		String metro6[] = StoreStation.makeArrayMetro6();
-		
+
 		Container contentPane = getContentPane();
 		contentPane.setLayout(null);
-		
+
 		contentPane.setBackground(Color.ORANGE);
-				
+
 		JComboBox<String> linenum = new JComboBox<String>(metroLine); //호선 선택
 		linenum.setBounds(50, 50, 70, 30);
-		//linenum.setOpaque(true);
 		contentPane.add(linenum);
-	
+
 		JLabel stname = new JLabel();
 		stname.setText("역:");
 		stname.setBounds(170, 50, 30, 30);
 		contentPane.add(stname);
-		
+
 		JTextField tf = new JTextField(10); //역 이름 입력
 		tf.setBounds(200, 50, 120, 30);
 		contentPane.add(tf);
-				
-		tf.addKeyListener(new KeyListener() {
-			String enteredStation;
+
+		JComboBox<String> mtdirection = new JComboBox<String>(metroDirection);
+		mtdirection.setBounds(370, 50, 70, 30);
+		contentPane.add(mtdirection);
+
+		JButton okay= new JButton();
+		okay.setText("입력");
+		okay.setBounds(210, 100, 70, 30);
+		contentPane.add(okay);
+
+		JLabel result = new JLabel();
+		
+		/*tf.addKeyListener(new KeyListener() {
 			public void keyPressed(KeyEvent e) {
 				if(e.getKeyCode()==KeyEvent.VK_ENTER) {
 					enteredStation = tf.getText();
 				}
 			}	//역 이름 입력하고 엔터하면 enteredStation에 역 이름 저장
-
 			@Override
-			public void keyReleased(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
+			public void keyReleased(KeyEvent arg0) {}
 			@Override
-			public void keyTyped(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
+			public void keyTyped(KeyEvent arg0) {}
+		});
+
+		mtdirection.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selectedDirection = mtdirection.getSelectedIndex();
 			}
 		});
 		
-		
-		JComboBox<String> mtdirection = new JComboBox<String>(metroDirection);
-		mtdirection.setBounds(370, 50, 70, 30);
-		contentPane.add(mtdirection);
-		
-		
+		linenum.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selectedLine = linenum.getSelectedIndex();
+			}
+		});*/
+
+		okay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JButton b = (JButton)e.getSource();
+				if(b.getText().equals("입력")) {
+					
+					selectedDirection = linenum.getSelectedIndex();
+					
+					//JComboBox sll = (JComboBox)e.getSource();
+					selectedLine = mtdirection.getSelectedIndex();
+					
+					enteredStation = tf.getText();
+					
+					
+					if(selectedLine == 1) {
+						int num = Search.searchClosest(enteredStation, selectedDirection, metro3, toiletinst3);
+						result.setText(metro3[num]);
+						result.setBounds(200, 180, 100, 30);
+						contentPane.add(result);
+						System.out.println(result);
+					}
+					
+					else if(selectedLine == 2) {
+						int num2 = Search.searchClosest(enteredStation, selectedDirection, metro6, toiletinst6);
+						result.setText(metro6[num2]);
+						result.setBounds(200, 170, 100, 30);
+						contentPane.add(result);
+						System.out.println(result);
+					}
+					
+				}
+
+			}
+	});		
+
+		/*if(selectedLine == 1) {
+			JLabel result = new JLabel();
+			result.setText(Search.searchClosest(enteredStation, selectedDirection, metro3, toiletinst3));
+			result.setBounds(100, 100, 70, 30);
+			contentPane.add(result);
+		}*/
 
 		setTitle("Toilet in Station");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		setSize(500, 300);
-		setVisible(true);
-		
-	}
+		setVisible(true);	
+
+}
 }
 
-
-
 class Search{
+	//n을 for문 밖으로 꺼냈을 떄도 i와 값이 같아야하는데 이를 고쳐야하뮤ㅠ
 	
-	static void searchClosest(int metroline, String station, String direction, String [] metro, int [] toiletinst) {
-		int n;
+	static int searchClosest(String station, int selectedDirection, String [] metro, int [] toiletinst) {
+		int n=0;
 		int min = 100;
-
-		if(direction.equals("상행선")) {
+		
+		if(selectedDirection == 1 || selectedDirection == 3) {
 			for(int i =0; i<metro.length; i++) {
 				if(metro[i].equals(station)) {
 					n = i;
@@ -122,12 +174,14 @@ class Search{
 						else if(toiletinst[j] - n == 0)
 							min = 0;
 					}
-					System.out.println(metro[n-min]);
+					//System.out.println(metro[n-min]);
 				}
 
 			}
+			return n-min;
 		}
-		else if(direction.equals("하행선")) {
+
+		else if(selectedDirection == 2 || selectedDirection == 4) {
 			for(int i =0; i<metro.length; i++) {
 				if(metro[i].equals(station)) {
 					n = i;
@@ -137,27 +191,30 @@ class Search{
 						else if(toiletinst[j] - n == 0)
 							min = 0;
 					}
-					System.out.println(metro[n+min]);
+					//return metro[n+min];
+				//	System.out.println(metro[n+min]);
 				}
 
 			}
-
+		return n+min;
 		}
+		else 
+		return 0;
 	}
 }
 
 
 public class MetroToiletMain{
-	
+
 	static int toiletinst3[] = {5, 11, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 35, 37, 40, 41, 42}; 
 	static int toiletinst6[] = {9, 10, 22, 28, 29, 30, 31, 35};
-	
+
 	public static void main(String[] args) {
-		
+
 		new UI();
 		String metro3[] = StoreStation.makeArrayMetro3();
 		String metro6[] = StoreStation.makeArrayMetro6();
-		
+
 		/*static String metro3[] = new String[43];
 		static String metro6[] = new String[39];
 
@@ -185,7 +242,7 @@ public class MetroToiletMain{
 			System.exit(0);
 		}*/
 		//-----------------↑3호선, 6호선 배열 정리↑---------------------------------
-		
+
 		/*Scanner a = new Scanner(System.in);
 		System.out.print("현재 탑승한 지하철 호선: ");
 		int metroline = a.nextInt();
@@ -193,12 +250,12 @@ public class MetroToiletMain{
 		String station = a.next();
 		System.out.print("상행선(내선) or 하행선(외선): ");
 		String direction = a.next();
-		*/
-		
+		 */
+
 		/*if(metroline == 3)
 			Search.searchClosest(metroline, station, direction, metro3, toiletinst3);*/
 
-		
+
 
 	}
 
