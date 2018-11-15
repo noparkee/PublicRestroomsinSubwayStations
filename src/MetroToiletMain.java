88770152555e6a186ce8bc9a1f714277470e09c3
unknown <noparkee@naver.com>
2018-11-15 22:08:19 +0900

    modify direction

--- src/MetroToiletMain.java
+++ src/MetroToiletMain.java
@@ -18,6 +18,7 @@ class Examine{
 			return 0; //잘 못 입력
 	}
 }
+
 class StoreStation {
 	static String[] makeArrayMetro3() {
 		String metro3[] = new String[43];
@@ -52,7 +53,6 @@ class StoreStation {
 	}
 }
 
-
 class UI extends JFrame{
 	static int toiletinst3[] = {5, 11, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 35, 37, 40, 41, 42}; 
 	static int toiletinst6[] = {9, 10, 22, 28, 29, 30, 31, 35};
@@ -64,7 +64,10 @@ class UI extends JFrame{
 	public UI() {
 
 		String metroLine[] = {"호선","3호선","6호선"};
-		String metroDirection[] = {"방향","상행선 (내선순환)", "하행선 (외선순환)"};
+		Vector<String> stationSE = new Vector<String>();
+		stationSE.add("방향");
+
+		//String metroDirection[] = {"방향","상행선 (내선순환)", "하행선 (외선순환)"};
 		String metro3[] = StoreStation.makeArrayMetro3();
 		String metro6[] = StoreStation.makeArrayMetro6();
 
@@ -77,22 +80,52 @@ class UI extends JFrame{
 		linenum.setBounds(50, 50, 70, 30);
 		contentPane.add(linenum);
 
+		//linenum.addItemListener(new MyItemListener());
+		linenum.addActionListener(new ActionListener() {	//처음에 선택할때는 옳게 뜨는데 두 번 이상 호선을 바꾸면 선택하면 다르게 뜸. ***여기 수정
+			public void actionPerformed(ActionEvent e) {
+				JComboBox<String> sl = (JComboBox<String>)e.getSource();
+				int num = sl.getSelectedIndex();
+				//Object obj = e.getItem();
+				switch (num) {
+				case 1: 
+					if(stationSE.get(1).isEmpty()) {
+						stationSE.removeAllElements();
+						stationSE.add("방향");
+					}
+					
+					stationSE.add(1, metro3[0] + "행");
+					stationSE.add(2, metro3[metro3.length-1] + "행");
+					// System.out.println(stationSE.get(2));
+					break;
+				case 2:
+					stationSE.add(1, metro6[0] + "행");
+					stationSE.add(2, metro6[metro6.length-1] + "행");
+					break;
+				default:
+					break;
+				}
+			}
+		}
+
+				);
+
 		JLabel stname = new JLabel();
 		stname.setText("역:");
-		stname.setBounds(150, 50, 30, 30);
+		stname.setBounds(140, 50, 30, 30);
 		contentPane.add(stname);
 
 		JTextField tf = new JTextField(10); //역 이름 입력
-		tf.setBounds(180, 50, 120, 30);
+		tf.setBounds(170, 50, 120, 30);
 		contentPane.add(tf);
 
-		JComboBox<String> mtdirection = new JComboBox<String>(metroDirection);
-		mtdirection.setBounds(330, 50, 110, 30);
+		JComboBox<String> mtdirection = new JComboBox<String>(stationSE);
+		//JComboBox<String> mtdirection = new JComboBox<String>(metroDirection);
+		mtdirection.setBounds(320, 50, 130, 30);
 		contentPane.add(mtdirection);
 
 		JButton okay= new JButton();
 		okay.setText("입력");
-		okay.setBounds(210, 100, 70, 30);
+		okay.setBounds(190, 100, 70, 30);
 		contentPane.add(okay);
 
 		JLabel result = new JLabel();
@@ -110,14 +143,13 @@ class UI extends JFrame{
 					enteredStation = tf.getText();
 
 					if(selectedLine == 1) {
-
 						int num = Search.searchClosest(enteredStation, selectedDirection, metro3, toiletinst3);
 						if(num!=1220) {
 							if(Examine.rightLS(metro3, enteredStation)==1) {
-							result.setText(metro3[num]);
-							result.setBounds(215, 170, 70, 30);
-							contentPane.add(result);
-							//System.out.println(result);
+								result.setText(metro3[num]);
+								result.setBounds(215, 170, 70, 30);
+								contentPane.add(result);
+								//System.out.println(result);
 							}
 							else {
 								result.setText("입력을 확인해주세요.");
@@ -135,10 +167,10 @@ class UI extends JFrame{
 
 						int num = Search.searchClosest(enteredStation, selectedDirection, metro6, toiletinst6);
 						if(num!=1220) {
-							if(Examine.rightLS(metro3, enteredStation)==1) {
-							result.setText(metro6[num]);
-							result.setBounds(200, 170, 100, 30);
-							contentPane.add(result);
+							if(Examine.rightLS(metro6, enteredStation)==1) {
+								result.setText(metro6[num]);
+								result.setBounds(200, 170, 100, 30);
+								contentPane.add(result);
 							}
 							else {
 								result.setText("입력을 확인해주세요.");
@@ -159,7 +191,6 @@ class UI extends JFrame{
 
 			}
 		});		
-
 
 		setTitle("Toilet in Station");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -201,7 +232,7 @@ class Search{
 						else if(toiletinst[j] - n == 0)
 							min = 0;
 					}
-					}
+				}
 
 			}
 			return n+min;
